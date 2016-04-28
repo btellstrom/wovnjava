e7287ffd87f959bb25530d43be73ef8c8dc97b77
Masahiro Iuchi <iuchi@wovn.io>
2016-04-28 11:10:21 +0900

    Version 0.0.1.

--- src/main/java/com/github/wovnio/wovnjava/WovnServletFilter.java
+++ src/main/java/com/github/wovnio/wovnjava/WovnServletFilter.java
@@ -16,7 +16,7 @@ import org.apache.commons.logging.LogFactory;
 public class WovnServletFilter implements Filter {
     protected Interceptor interceptor;
 
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.0.1";
 
     static final Log log = LogFactory.getLog(WovnServletFilter.class);
 
