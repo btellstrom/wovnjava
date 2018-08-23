31c08c1fb62318a91f78daf024b08a22416277c3
kyuden <msmsms.um@gmail.com>
2018-08-23 15:44:42 +0900

    Release 0.3.9

--- src/main/java/com/github/wovnio/wovnjava/Settings.java
+++ src/main/java/com/github/wovnio/wovnjava/Settings.java
@@ -11,7 +11,7 @@ import javax.servlet.FilterConfig;
 import javax.xml.bind.DatatypeConverter;
 
 class Settings {
-    public static final String VERSION = "0.3.8";
+    public static final String VERSION = "0.3.9";
     static final String UrlPatternRegPath = "/([^/.?]+)";
     static final String UrlPatternRegQuery = "(?:(?:\\?.*&)|\\?)wovn=([^&]+)(?:&|$)";
     static final String UrlPatternRegSubdomain = "^([^.]+)\\.";
