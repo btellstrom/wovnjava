76d5bf9245fdd59d713b3ff70c96b137888458c3
Torkel Berli <torkelbe@gmail.com>
2019-03-06 14:45:21 +0900

    Add debug HTTP headers

--- src/main/java/com/github/wovnio/wovnjava/WovnServletFilter.java
+++ src/main/java/com/github/wovnio/wovnjava/WovnServletFilter.java
@@ -30,14 +30,18 @@ public class WovnServletFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException
     {
+        ((HttpServletResponse)response).setHeader("X-Wovn-Top", "init");
         Headers headers = new Headers((HttpServletRequest)request, settings);
         String lang = headers.getPathLang();
         boolean hasShorterPath = settings.urlPattern.equals("path") && lang.length() > 0 && lang.equals(settings.defaultLang);
         if (hasShorterPath) {
+            ((HttpServletResponse)response).setHeader("X-Wovn-Top", "redirect!");
             ((HttpServletResponse) response).sendRedirect(headers.redirectLocation(settings.defaultLang));
         } else if (htmlChecker.canTranslatePath(headers.pathName)) {
+            ((HttpServletResponse)response).setHeader("X-Wovn-Top", "try-trans!");
             tryTranslate(headers, (HttpServletRequest)request, response, chain);
         } else {
+            ((HttpServletResponse)response).setHeader("X-Wovn-Top", "pass!");
             WovnHttpServletRequest wovnRequest = new WovnHttpServletRequest((HttpServletRequest)request, headers);
             chain.doFilter(wovnRequest, response);
         }
@@ -48,44 +52,60 @@ public class WovnServletFilter implements Filter {
     }
 
     private void tryTranslate(Headers headers, HttpServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "init");
         WovnHttpServletRequest wovnRequest = new WovnHttpServletRequest(request, headers);
         WovnHttpServletResponse wovnResponse = new WovnHttpServletResponse((HttpServletResponse)response, headers);
 
         if (settings.urlPattern.equals("path") && headers.getPathLang().length() > 0) {
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "forward");
+            ((HttpServletResponse)response).setHeader("X-Wovn-Forward", headers.pathNameKeepTrailingSlash);
             headers.trace("WovnServletFilter#forward to " + headers.pathNameKeepTrailingSlash);
             wovnRequest.getRequestDispatcher(headers.pathNameKeepTrailingSlash).forward(wovnRequest, wovnResponse);
         } else {
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "call-chain");
             headers.trace("WovnServletFilter#chain.doFilter");
             chain.doFilter(wovnRequest, wovnResponse);
         }
+        ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "response-received");
 
         headers.trace("WovnServletFilter#Response received");
         String originalBody = wovnResponse.toString();
         if (originalBody != null) {
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "is-text");
             // text
             String body = null;
             if (htmlChecker.canTranslate(response.getContentType(), headers.pathName, originalBody)) {
+                ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "text-can-translate");
                 // html
                 Api api = new Api(settings, headers);
+                ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "text-api");
                 Interceptor interceptor = new Interceptor(headers, settings, api);
                 body = interceptor.translate(originalBody);
+                ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "text-intercepted");
 
                 // Temporary debugging section
                 headers.trace("WovnServletFilter#translation complete");
                 String debugging_section = createDebuggingSection(request, wovnResponse, headers);
                 body = body + debugging_section;
             } else {
+                ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "no-text");
                 // css, javascript or others
                 body = originalBody;
             }
+
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "text-body-ready");
             wovnResponse.setContentLength(body.getBytes().length);
             wovnResponse.setCharacterEncoding("utf-8");
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "text-writer-fetching");
             PrintWriter out = response.getWriter();
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "text-write!");
             out.write(body);
             out.close();
         } else {
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "is-binary");
             // binary
             ServletOutputStream out = response.getOutputStream();
+            ((HttpServletResponse)response).setHeader("X-Wovn-Trans", "binary-write!");
             out.write(wovnResponse.getData());
             out.close();
         }
